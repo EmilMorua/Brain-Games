b42030eb861eebfe1ca9d8abc6016f794cfca7c3
Emil Murzin <emil.morua@gmail.com>
2022-11-28 13:35:30 +0300

    connection prompt

--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,8 +1,13 @@
 #!/Users/chibis/code/Projects/Project 1/python-project-49/.venv/bin python3
 
-def main():
+from brain_games.cli import welcome_user
+
+def greet():
     print("Welcome to the Brain Games!")
 
+def main():
+    greet()
+    welcome_user()
 
 if __name__ == '__main__':
-    main()
+    greet()
